a420567d6660f04b1be4bf1367699fd45197d755
IP <imranypatel@gmail.com>
2014-04-12 08:25:04 -0700

    Setting up

---
+++ App/CMSLITE/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CMSLITE.Repository;
+
+namespace CMSLITE.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        PagesDataContext ctxpag = new PagesDataContext();
+   
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+
+            var page = ctxpag.Pages;
+                         
+            return View(page);
+            
+        }
+
+        public ActionResult PageEditor()
+        {
+            var pageEditor = ctxpag.PageEditors;
+
+            return PartialView("_PageEditor",pageEditor);
+        }
+
+    }
+}
---
+++ Integ/ipCMSLite.sql
@@ -0,0 +1,14 @@
+SElect *
+From PageEditors
+
+SElect *
+From Pages
+
+SElect *
+From PageCOmposition
+
+SElect *
+From PageParts
+
+Select *
+From PagePartComposition
